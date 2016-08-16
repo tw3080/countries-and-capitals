5c7cf72189f91442767f0841624bd83c8fb56195
Taylor Welling <tw3080@gmail.com>
2016-08-16 11:45:21 -0700

    set .data = response

--- app/countries/countries.js
+++ app/countries/countries.js
@@ -1,5 +1,12 @@
 viewsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/countries', {
-        templateUrl: 'countries/countries.html'
+        templateUrl: 'countries/countries.html',
+        controller: 'CountriesCtrl'
     });
 }]);
+
+viewsModule.controller('CountriesCtrl', function($scope, getCountryInfo) {
+    getCountryInfo().then(function(response) {
+        $scope.data = response;
+    });
+});
