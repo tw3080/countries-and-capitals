056b1f7df9240aeae35a104f495da3c1aa8a72e9
Taylor Welling <tw3080@gmail.com>
2016-08-16 15:07:17 -0700

    trying to get the desire response from countrySearch

--- app/library.js
+++ app/library.js
@@ -2,6 +2,7 @@ angular.module('countriesLibrary', [])
 .constant('GEONAMES_API_PREFIX', 'http://api.geonames.org')
 .constant('GEONAMES_TYPE', 'json')
 .constant('GEONAMES_USERNAME', 'tw3080')
+// TODO: Promises stuff? $q
 .factory('getCountryInfo', ['$http', '$q', 'GEONAMES_API_PREFIX', 'GEONAMES_TYPE', 'GEONAMES_USERNAME', function($http, $q, GEONAMES_API_PREFIX, GEONAMES_TYPE, GEONAMES_USERNAME) {
     return function() {
         var params = {
@@ -11,6 +12,23 @@ angular.module('countriesLibrary', [])
         return $http({
             method: 'GET',
             url: GEONAMES_API_PREFIX + '/countryInfo',
+            cache: true,
+            params: params
+        })
+        .then(function(response) {
+            return response.data.geonames;
+        });
+    };
+}])
+.factory('countrySearch', ['$http', '$q', 'GEONAMES_API_PREFIX', 'GEONAMES_TYPE', 'GEONAMES_USERNAME', function($http, $q, GEONAMES_API_PREFIX, GEONAMES_TYPE, GEONAMES_USERNAME) {
+    return function() {
+        var params = {
+            username: GEONAMES_USERNAME,
+            type: GEONAMES_TYPE
+        };
+        return $http({
+            method: 'GET',
+            url: GEONAMES_API_PREFIX + '/search',
             cache: true,
             params: params
         })
