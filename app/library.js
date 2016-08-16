eba1f865c35ce126b6a712168b72b20baf7b3461
Taylor Welling <tw3080@gmail.com>
2016-08-15 19:37:09 -0700

    added constants

--- app/library.js
+++ app/library.js
@@ -1,2 +1,4 @@
 angular.module('countriesLibrary', [])
-    .constant('')
+    .constant('GEONAMES_API_PREFIX', 'http://api.geonames.org')
+    .constant('GEONAMES_TYPE', 'json')
+    .constant('GEONAMES_USERNAME', 'tw3080')
