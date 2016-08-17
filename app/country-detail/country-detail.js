959c308af945c3749a4e65954755e6b183ac00ae
Taylor Welling <tw3080@gmail.com>
2016-08-17 14:08:39 -0700

    removed unnecessary console logs

--- app/country-detail/country-detail.js
+++ app/country-detail/country-detail.js
@@ -8,20 +8,14 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
 viewsModule.controller('CountryDetailCtrl', function($scope, $routeParams, getCountryInfo, $filter, countrySearch, neighborSearch) {
     $scope.countryCode = $routeParams.countryCode;
     getCountryInfo().then(function(response) {
-        // console.log(response);
         var country = $filter('getByCountryCode')(response, $scope.countryCode);
         $scope.countryData = country;
-        // console.log(country);
         return countrySearch(country.capital);
     }).then(function(response) {
-        console.log(response[0]);
         $scope.capitalData = response[0];
-        // $scope.capital = $scope.capitalData;
         return neighborSearch($scope.capitalData.countryCode);
     }).then(function(response) {
-        // console.log(response);
         $scope.neighbors = response;
-        console.log($scope.neighbors);
     });
 
     $scope.goToNeighbor = function(code) {
