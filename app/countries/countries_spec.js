97afbd84fb445950687d6c08844232634d25617c
Taylor Welling <tw3080@gmail.com>
2016-08-19 14:50:16 -0700

    moved the controller description into a separate 'describe' block

--- app/countries/countries_spec.js
+++ app/countries/countries_spec.js
@@ -2,16 +2,20 @@ describe('countriesAppViews', function() {
     beforeEach(module('countriesAppViews'));
 
     describe('/countries route', function() {
-        it('should load the template', inject(function($location, $httpBackend, $rootScope, $route) {
+        it('should load the template and the controller', inject(function($location, $httpBackend, $rootScope, $route) {
             $httpBackend.whenGET('countries/countries.html').respond('...');
             $httpBackend.expectGET('//api.geonames.org/countryInfo?type=json&username=tw3080').respond({});
+
             $rootScope.$apply(function() {
                 $location.path('/countries');
             });
             expect($route.current.loadedTemplateUrl).toBe('countries/countries.html');
+            expect($route.current.controller).toBe('CountriesCtrl');
         }));
+    });
 
-        beforeEach(inject(function($controller, $rootScope, _$location_) {
+    describe('CountriesCtrl', function() {
+        beforeEach(inject(function($rootScope, $controller, _$location_) {
             scope = $rootScope.$new();
             ctrl = $controller('CountriesCtrl', {
                 $scope: scope
